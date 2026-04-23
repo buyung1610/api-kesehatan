c719cc1acedbb4d36658b887cac5c70a20b9b59a
Fajrulll <123521836+buyung1610@users.noreply.github.com>
2026-04-23 14:40:15 +0700

    update node-cron

--- src/index.js
+++ src/index.js
@@ -12,25 +12,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // Folder statis untuk melihat gambar
-app.use("/uploads", express.static(path.join(__dirname, "uploads"), {
-  setHeaders: (res, filePath) => {
-    if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) {
-      res.setHeader("Content-Type", "image/jpeg");
-    } else if (filePath.endsWith(".png")) {
-      res.setHeader("Content-Type", "image/png");
-    }
-  }
-}));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) {
+        res.setHeader("Content-Type", "image/jpeg");
+      } else if (filePath.endsWith(".png")) {
+        res.setHeader("Content-Type", "image/png");
+      }
+    },
+  }),
+);
 
 // Job jalan tiap jam 12
-cron.schedule('30 14 * * *', async () => {
+cron.schedule("0 0 * * *", async () => {
   console.log("Cron jalan:", new Date().toLocaleString());
   try {
     await seedArticles(); // tunggu sampai selesai
@@ -39,6 +44,7 @@ cron.schedule('30 14 * * *', async () => {
   }
 });
 
+await seedArticles();
 
 const port = process.env.PORT;
 const host = process.env.HOST || "localhost";
